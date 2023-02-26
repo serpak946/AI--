a2c4aecc63dd395c41e4fad84c2ebd87b7a88abf
serpak <63670242+serpak946@users.noreply.github.com>
2023-02-26 15:02:53 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import openai
-openai.api_key = "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"
-
-prompt = "What is the meaning of life?"
-model = "text-davinci-002"
-response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=50)
-print(response.choices[0].text)
